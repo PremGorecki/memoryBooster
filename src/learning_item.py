120f87033d283ec610d9dd34368d80ae08ebe039
Przemysław Górecki <minutnik75@gmail.com>
2024-05-01 08:40:40 +0200

    Code review changes - part 3

--- src/learning_item.py
+++ src/learning_item.py
@@ -13,16 +13,3 @@ class LearningItem:
         self.number_of_correct_repetitions = number_of_correct_repetitions
         self.number_of_incorrect_repetitions = number_of_incorrect_repetitions
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "is_taught": self.is_taught,
-            "is_verb_irregular": self.is_verb_irregular,
-            "questions": self.questions,
-            "answers": self.answers,
-            "interval": self.interval,
-            "repetition_date": self.repetition_date,
-            "easiness_factor": self.easiness_factor,
-            "number_of_correct_repetitions": self.number_of_correct_repetitions,
-            "number_of_incorrect_repetitions": self.number_of_incorrect_repetitions
-        }
--- src/learning_item_data_retriever.py
+++ src/learning_item_data_retriever.py
@@ -4,23 +4,27 @@ from learning_item import LearningItem
 
 FOLDER_PATH = "E:\\Mi unidad\\_PYTHON\\PycharmProjects\\memoryBooster\\data\\"
 SOURCE_FILE = "data_esp.txt"
-RESULT_FILE = "data_esp2.json"
+RESULT_FILE = "data_esp.json"
 
-def learning_items_from_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
 
-    learning_items = [ ]
-    number_of_units = int(lines[0])
-    num_lines = len(lines)
+def parse_boolean(line):
+    return line.strip() == "true"
+
+
+def parse_int(line):
+    return int(line.strip())
+
+
+def parsing_items_from_lines(lines, num_lines):
+    learning_items = []
     i = 2
     while (i < num_lines):
         if lines[i].strip() == '-' * len(lines[i].strip()):
             i += 1
             continue
-        id = int(lines[i].strip())
-        isTaught = lines[i + 1].strip() == "true"
-        isVerbIrregular = lines[i + 2].strip() == "true"
+        id = parse_int(lines[i])
+        isTaught = parse_boolean(lines[i + 1])
+        isVerbIrregular = parse_boolean(lines[i + 2])
         questions = []
         for j in range(3, 6):
             if lines[i + j].strip():
@@ -29,47 +33,62 @@ def learning_items_from_file(file_path):
         for j in range(6, 13):
             if lines[i + j].strip():
                 answers.append(lines[i + j].strip())
-        interval = int(lines[i + 13].strip())
+        interval = parse_int(lines[i + 13])
         repetitionDate = lines[i + 14].strip()
-        easinessFactor = round(float(lines[i + 15].strip()),4)
-        numberOfCorrectRepetitions = int(lines[i + 16].strip())
-        numberOfIncorrectRepetitions = int(lines[i + 17].strip())
+        easinessFactor = round(float(lines[i + 15].strip()), 4)
+        numberOfCorrectRepetitions = parse_int(lines[i + 16])
+        numberOfIncorrectRepetitions = parse_int(lines[i + 17])
         learning_item = LearningItem(id, isTaught, isVerbIrregular, questions, answers,
-                                           interval, repetitionDate, easinessFactor,
-                                           numberOfCorrectRepetitions, numberOfIncorrectRepetitions)
+                                     interval, repetitionDate, easinessFactor,
+                                     numberOfCorrectRepetitions, numberOfIncorrectRepetitions)
         learning_items.append(learning_item)
         i += 18
-
     return learning_items
+
+
+def learning_items_from_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    number_of_units = int(lines[0])
+    num_lines = len(lines)
+
+    return parsing_items_from_lines(lines, num_lines)
+
 
 def json_from_learning_items_to_file(learning_elements, file_path):
     with open(file_path, 'w', encoding='utf-8') as file:
-        json.dump([item.to_dict() for item in learning_items], file, indent=4, ensure_ascii=False)
+        json.dump([item.__dict__ for item in learning_items], file, indent=4, ensure_ascii=False)
 
 
-def show_items(learning_items, amount, is_difficutl):
+def hardest_questions(df, amount):
+    result = (df.nlargest(amount, 'number_of_incorrect_repetitions')
+              .rename(columns={"number_of_incorrect_repetitions": "IncRep"}))
+    print("====The most difficult items=================")
+    print(result[['questions', 'answers', 'IncRep']])
+
+
+def easiest_questions(df, amount):
+    result = (((df[df.number_of_incorrect_repetitions == 0]
+                .sort_values('number_of_incorrect_repetitions', ascending=False))
+               .nlargest(amount, 'number_of_correct_repetitions'))
+              .rename(columns={"number_of_correct_repetitions": "CorRep"}))
+    print("====The easies items=========================")
+    print(result[['questions', 'answers', 'CorRep']])
+
+
+def show_items(learning_items, amount, is_hardest):
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
     pd.set_option('display.expand_frame_repr', False)
-    data = [item.to_dict() for item in learning_items]
+    data = [item.__dict__ for item in learning_items]
     df = pd.DataFrame(data)
-    if (is_difficutl):
-        result = (df.nlargest(amount, 'number_of_incorrect_repetitions')
-                  .rename(columns={"number_of_incorrect_repetitions": "IncRep"}))
-        print("====The most difficult items=================")
-        print(result[['questions', 'answers', 'IncRep']])
-
+    if is_hardest:
+        hardest_questions(df, amount)
     else:
-        #Fifteen the easiest items to lear
-        result = (((df[df.number_of_incorrect_repetitions == 0]
-                     .sort_values('number_of_incorrect_repetitions', ascending=False))
-                     .nlargest(amount, 'number_of_correct_repetitions'))
-                  .rename(columns={"number_of_correct_repetitions": "CorRep"}))
-        print("====The easies items=========================")
-        print(result[['questions', 'answers', 'CorRep']])
+        easiest_questions(df, amount)
 
 
 learning_items = learning_items_from_file(FOLDER_PATH + SOURCE_FILE)
 json_from_learning_items_to_file(learning_items, FOLDER_PATH + RESULT_FILE)
-show_items(learning_items, 15, True)
-
+show_items(learning_items, 15, False)
